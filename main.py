251bbfc7ef25c1dc0476fae05ce8177be582d046
yural64 <yural64@gmail.com>
2024-10-21 18:37:59 +0300

    check 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import requests
+
+# Создаем переменную, куда будут сохраняться результаты запроса
+# response = requests.get("https://www.google.com/")
+# print(response.status_code)
+# if response.ok:
+#     print("Запрос успешно выполнен")
+# else:
+#     print("Произошла ошибка")
