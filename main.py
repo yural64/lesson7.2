fff9fd4d83998c6c6a849828c20d32dccd6f6acd
yural64 <yural64@gmail.com>
2024-10-21 22:10:07 +0300

    edit code1

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ params1 = {
 response1 = requests.get(url1, params=params1)
 response1_json = response1.json()
 
-print(response1.status_code)
+print(f"Статус-код задания 1: {response1.status_code}")
+print(f"JSON-формат задания 1: ")
 pprint.pprint(response1_json)
+print()
+
